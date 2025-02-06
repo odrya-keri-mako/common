525a5aa8edb4ceb7b4c3552c268affc1d4c547ad
odrya-keri-mako <odry.attila@keri.mako.hu>
2025-02-06 16:53:07 +0100

    default.php comments

--- php/default.php
+++ php/default.php
@@ -4,20 +4,40 @@ declare(strict_types=1);
 // Include environment
 require_once("./environment.php");
 
-// Get arguments
+/**
+ * Get arguments (data)
+ * Only needed, when client side sends data to server
+ */
 $args = Util::getArgs();
 
-// Connect to MySQL server
-$db = new Database();
+/**
+ * Connect to MySQL server
+ * Alternative: 
+ * 		$db = new Database('dbname');
+ */
+$db = new Database(); 
 
-// Set SQL command
+/**
+ * Set query
+ * Set arguments (cannot be mixed): 
+ * 		- ? 
+ * 		-	:key
+ */
 $query = "";
 
-// Execute SQL command
+/**
+ * Execute SQL command
+ */ 
 $result = $db->execute($query, $args);
 
 // Close connection
 $db = null;
 
-// Ser response
+/**
+ * Check response (result)
+ * 
+ * Util::setError('Error message!');
+ */ 
+
+// Set response
 Util::setResponse($result);
