92d3a49a3afcd276328e69eb1101f36ea7ad64de
odrya-keri-mako <odry.attila@keri.mako.hu>
2025-03-10 04:24:32 +0100

    user properties

--- js/app-user.js
+++ js/app-user.js
@@ -16,8 +16,9 @@
     'msg',
     ($rootScope, $state, util, msg) => {
 
-      // Set user properties
-      let properties;
+      // Set user properties, and root scope user key
+      let properties,
+          userKey;
 
       // Set local methods
       let methods = {
@@ -40,21 +41,36 @@
       let user = {
 
         // Initialize
-        init: (prop=null) => {
+        init: (prop=null, key=null) => {
 
           // Check user properties
+          if (util.isString(prop)) {
+            prop = prop.replaceAll(",", ";");
+            prop = prop.replaceAll(" ", "");
+            prop = prop.split(';');
+          }
           if (util.isObject(prop))
                 properties = util.objMerge({}, prop);
+          else if (util.isArray(prop) && prop.length)
+                properties = prop.reduce((o, k) => (o[k] = null, o), {});
           else  properties = util.objMerge({}, methods.default());
 
+          // Check root scope user key
+          if (util.isString(key))
+            key = key.replaceAll(" ", "");
+          if (util.isString(key) && key.length)
+                userKey = key;
+          else  userKey = 'user';
+
           // Set user default properties
-          $rootScope.user = util.objMerge({}, properties);
+          $rootScope[userKey] = util.objMerge({}, properties);
         },
 
         // Set
         set: (data) => {
           Object.keys(properties).forEach(key => {
-            if (util.hasKey(data, key)) $rootScope.user[key] = data[key];
+            if (util.hasKey(data, key)) 
+              $rootScope[userKey][key] = data[key];
           });
           $rootScope.$applyAsync();
         },
@@ -64,10 +80,14 @@
 
         // Reset
         reset: (filter=null) => {
-          if (util.isString(filter)) filter = filter.split(',');
+          if (util.isString(filter)) {
+            filter = filter.replaceAll(",", ";");
+            filter = filter.replaceAll(" ", "");
+            filter = filter.split(';');
+          }
           if (!util.isArray(filter)) filter = [];
-          Object.keys($rootScope.user).forEach(key => {
-            if (!filter.includes(key)) $rootScope.user[key] = null;
+          Object.keys(properties).forEach(key => {
+            if (!filter.includes(key)) $rootScope[userKey][key] = null;
           });
           $rootScope.$applyAsync();
         }
@@ -81,10 +101,14 @@
           isConfirm	: true,
           callback  : (response) => {
             if (response === 'ok') {
-              user.reset('email');
-              if (util.isObjectHasKey($rootScope, 'state') &&
-                  $rootScope.state.disabled.includes($rootScope.state.id))
-                $state.go($rootScope.state.default);
+              user.reset();
+              if (util.isObjectHasKey($rootScope, 'state')) {
+                if ($rootScope.state.disabled.includes($rootScope.state.id)) {
+                  if ($rootScope.state.disabled.includes($rootScope.state.prev))
+                        $state.go($rootScope.state.default);
+                  else  $state.go($rootScope.state.prev); 
+                }
+              }
             }
           }
         });
